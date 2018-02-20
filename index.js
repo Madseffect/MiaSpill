a045e09b9f0d67fe437525113254be86e39e68be
Mads Kristensen <mkr@alpha-solutions.dk>
2018-02-20 12:13:14 +0100

    hjj

--- index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    var file = "*/amount.txt"
-    var amount="";
-    file.open('r');
-    while (!file.eof) {
-        amount += file.readln() + "\n";
-    }
-    $("#amount").html(amount);
-})
